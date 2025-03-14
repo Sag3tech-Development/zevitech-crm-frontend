9fb68150e4765af3b9cb59e9780a4dc91c75375a
SyeddHassan <syeddhassanali@gmail.com>
2025-03-14 14:42:22 +0500

    fix build errors

--- src/app/(authentication)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SystemProvider } from "@/contexts/system-context";
-
-export default function AuthenticationLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <SystemProvider>{children}</SystemProvider>;
-}
